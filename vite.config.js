388657022cda7214581ccb45ff4a6d3ec40a2710
GFerreiroS <info@gferreiro.com>
2024-09-21 20:32:37 +0200

    Finished docker support and fixed tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import tailwindcss from 'tailwindcss'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'dist'
-  },
   plugins: [vue(), vueJsx()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
